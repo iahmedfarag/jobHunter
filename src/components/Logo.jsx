806de197e10794934f2f148113a4ad3d40d3d600
iahmedfarag <ahmedfarag1k@gmail.com>
2023-05-03 21:57:35 +0300

    edits

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import logo from "../assets/images/logo.svg";
+import logo from "../assets/images/favicon.ico";
 
 const Logo = () => {
   return <img src={logo} alt="jobhunter logo" className="logo" />;
